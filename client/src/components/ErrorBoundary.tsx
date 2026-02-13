2e78b5cf8d9276dc32850ded2b0387fa58504789
nedo <nesple@gmail.com>
2026-02-13 18:49:31 +0100

    Fix: correctly access this.props.children in ErrorBoundary

--- client/src/components/ErrorBoundary.tsx
+++ client/src/components/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { Component, type ErrorInfo, type ReactNode } from 'react';
 
 interface Props {
     children: ReactNode;
@@ -51,7 +51,7 @@ class ErrorBoundary extends Component<Props, State> {
             );
         }
 
-        return this.children;
+        return this.props.children;
     }
 }
 
